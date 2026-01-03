1439261934241ad61d91019f625affdb396bc4f8
Sourya <souryaadhikary2911@gmail.com>
2026-01-03 23:40:42 +0530

    Changed the Asked ones

--- smart_contracts/hardhat.config.js
+++ smart_contracts/hardhat.config.js
@@ -1,6 +1,20 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("hardhat-gas-reporter");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.28",
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  gasReporter: {
+    enabled: true,
+    currency: "USD",     
+    showTimeSpent: true,
+  },
 };
